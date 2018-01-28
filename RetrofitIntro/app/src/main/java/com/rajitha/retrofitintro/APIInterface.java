b8356f22870a2f0165ff2ab10c13855a9d327caf
admin <rajitha@alliontechnologies.com>
2018-01-28 20:55:49 +0800

    ïnitial commit

---
+++ RetrofitIntro/app/src/main/java/com/rajitha/retrofitintro/APIInterface.java
@@ -0,0 +1,33 @@
+package com.rajitha.retrofitintro;
+
+import com.rajitha.retrofitintro.pojo.MultipleResource;
+import com.rajitha.retrofitintro.pojo.User;
+import com.rajitha.retrofitintro.pojo.UserList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by rajitha on 09/01/17.
+ */
+
+interface APIInterface {
+
+    @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
